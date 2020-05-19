68f064270d389339c5265404d0d8db0887412c68
Rosa <Rosa@PC-DESPACHO>
2020-05-19 12:57:39 +0200

    hola

---
+++ ConVerEntornos/src/converentornos/Main.java
@@ -0,0 +1,12 @@
+
+package converentornos;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Persona p1 = new Persona(18, "Gonzalo", 83838322);
+        
+        p1.mostrarDatos();
+    }
+}
