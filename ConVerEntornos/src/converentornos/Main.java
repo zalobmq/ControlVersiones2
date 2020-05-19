f775d2ccb36dbf2f63c4d1b8ef08fde239cb14ed
zalobmq <60347734+zalobmq@users.noreply.github.com>
2020-05-19 13:14:26 +0200

    Update Main.java

--- ConVerEntornos/src/converentornos/Main.java
+++ ConVerEntornos/src/converentornos/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         
         Persona p1 = new Persona(18, "Gonzalo", 83838322);
-        
+       System.out.println(" ESTA ES LA MODIFICACION AÑADIDA");
         p1.mostrarDatos();
     }
 }
