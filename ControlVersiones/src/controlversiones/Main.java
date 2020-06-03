04c9ca1c6b58590a8ffa7179415307074355b6b6
Rosa <Rosa@PC-DESPACHO>
2020-06-03 13:59:52 +0200

    Realizo mi primer commit para el trabajo de control de versiones

---
+++ ControlVersiones/src/controlversiones/Main.java
@@ -0,0 +1,12 @@
+
+package controlversiones;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona(18, "Gonzalo", 611222333);
+ 
+        persona1.mostrarDatos();
+    }
+}
