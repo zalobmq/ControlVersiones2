61600ed99fb0d3d0b791f015e4e8adbb4a5ae5b0
zalobmq <60347734+zalobmq@users.noreply.github.com>
2020-06-03 16:42:37 +0200

    ya esta hecho el cambio

--- ControlVersiones/src/controlversiones/Main.java
+++ ControlVersiones/src/controlversiones/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         
         Persona persona1 = new Persona(18, "Gonzalo", 611222333);
-        System.out.println("este es el cambio que he realizado")
+        System.out.println("este es el cambio que he realizado");
         persona1.mostrarDatos();
     }
 }
